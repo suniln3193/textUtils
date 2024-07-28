30333a9e06d88d371ffdac7d023df88a619b8e9a
suniln3193 <suniln3193@gmail.com>
2024-07-28 23:54:23 +0530

    Navbar changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import Textarea from './components/Textarea';
 import AboutUs from './components/AboutUs'; // Ensure this component exists
 import './App.css';
 
-function App(props) {
+function App() {
   return (
     <Router>
       <div>
-        <nav
-          className="navbar navbar-expand-lg border-body fixed-top"
-          style={{ backgroundColor: '#4A148C', paddingTop: '15px', paddingBottom: '15px' }}
-        >
-          <div className="container">
-            <Link className="navbar-brand" to="/" style={{ color: '#fff', fontSize: '25px' }}>
-              TextUtils
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/" style={{ color: '#fff' }}>
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/about-us" style={{ color: '#fff' }}>
-                    About Us
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        {/* Content Here */}
+        <Navigation />
         <main style={{ marginTop: '90px' }}> {/* Adjust margin for fixed navbar */}
           <Routes>
             <Route path="/" element={<Textarea />} />
@@ -53,6 +17,56 @@ function App(props) {
         </main>
       </div>
     </Router>
+  );
+}
+
+function Navigation() {
+  const location = useLocation();
+
+  return (
+    <nav
+      className="navbar navbar-expand-lg border-body fixed-top"
+      style={{ backgroundColor: '#4A148C', paddingTop: '15px', paddingBottom: '15px' }}
+    >
+      <div className="container">
+        <Link className="navbar-brand" to="/" style={{ color: '#fff', fontSize: '25px' }}>
+          TextUtils
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item px-3">
+              <Link
+                className={`p-0 nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                to="/"
+                style={{ color: '#fff' }}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item px-3">
+              <Link
+                className={`p-0 nav-link ${location.pathname === '/about-us' ? 'active' : ''}`}
+                to="/about-us"
+                style={{ color: '#fff' }}
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
